43c28c77dd377bb8719ae4de4179759a4c2d21c8
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-04-21 03:32:38 +0100

    Migrations

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome';
+    return View('welcome');
+});
+
+Route::get('/allo', function () {
+    return 'allo';
 });
