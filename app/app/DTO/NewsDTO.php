dfff70df7af3797e119e8b35adbf460d1731407c
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-04-21 03:29:48 +0100

    adding Dto

---
+++ app/app/DTO/NewsDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DTO;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+readonly class CategoryDTO
+{
+    public function __construct(
+        public string $name,
+        public object $image
+    ){}
+
+    public static function fromRequest(StoreCategoryRequest|UpdateCategoryRequest $request)
+    {
+        $validatedData = $request->validated();
+        return new self(
+            name: $validatedData["name"],
+            image: $validatedData["image"]
+        );
+
+    }
+
+}
